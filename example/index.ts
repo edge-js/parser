ca1d3a932fdaaedf60fd8c6765dc83fea89ed17d
Harminder virk <virk.officials@gmail.com>
2020-03-26 16:26:14 +0530

    docs: update example code

--- example/index.ts
+++ example/index.ts
@@ -8,6 +8,30 @@
 */
 
 import { Parser } from '../index'
+import { EdgeBuffer } from '../index'
 
-const parser = new Parser({}, { filename: 'eval.edge' })
-console.log(parser.parse('Hello {{ username }}'))
+const filename = 'eval.edge'
+const parser = new Parser({})
+const buffer = new EdgeBuffer(filename)
+const ctx = {
+  escape (value: any) {
+    return value
+  },
+  reThrow (error: Error) {
+    throw error
+  },
+}
+
+parser.tokenize('Hello {{ username }}', filename).forEach((token) => parser.processToken(token, buffer))
+
+/**
+ * Compiled output
+ */
+const output = buffer.flush()
+console.log({ compiled: output })
+
+/**
+ * Wrap inside function and invoke it
+ */
+const fn = new Function('', `return function template (state, ctx) { ${output} }`)()
+console.log(fn({ username: 'virk' }, ctx))
