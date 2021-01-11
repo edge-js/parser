23d0aab8f9125e785b77a5a893909672fdd40579
Harminder Virk <virk.officials@gmail.com>
2021-01-11 19:23:46 +0530

    feat: add support for optional chaining

---
+++ fixtures/optional-chaining/index.js
@@ -0,0 +1,9 @@
+let out = "";
+let $lineNumber = 1;
+let $filename = "{{ __dirname }}index.edge";
+try {
+out += `${ctx.escape(state.team?.users.find(({age, name}) => age === state.minAge))}`;
+} catch (error) {
+ctx.reThrow(error, $filename, $lineNumber);
+}
+return out;
