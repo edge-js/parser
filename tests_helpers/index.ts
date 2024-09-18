af97f1623591c5054fc820ca1fe943b7488d1bd7
Harminder Virk <virk.officials@gmail.com>
2024-09-18 08:22:01 +0530

    chore(package): update dependencies

--- tests_helpers/index.ts
+++ tests_helpers/index.ts
@@ -7,13 +7,12 @@
  * file that was distributed with this source code.
  */
 
-import { sep } from 'node:path'
 import { EOL } from 'node:os'
-// @ts-ignore untyped module
+import { sep } from 'node:path'
+// @ts-expect-error "untyped module"
 import stringify from 'js-stringify'
 
 export function normalizeNewLines(value: string) {
-  // eslint-disable-next-line @typescript-eslint/quotes
   return value.replace(/out\s\+=\s"\\n"/g, `out += ${EOL === '\n' ? `"\\n"` : `"\\r\\n"`}`)
 }
 
